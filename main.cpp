6ff1ffcc4c8e21da8e39f47f770fb8dbea8e5645
Baptiste Pauchet <baptiste.pauchet13@gmail.com>
2026-03-03 18:56:07 +0100

    Mise en place du titre et icone

--- main.cpp
+++ main.cpp
@@ -4,8 +4,23 @@
 
 int main(int argc, char *argv[])
 {
+    #ifdef Q_OS_LINUX
+        qunsetenv("QT_QPA_PLATFORMTHEME");
+
+        qputenv("QT_QPA_PLATFORMTHEME", "xdgdesktopportal");
+    #endif
+
     QApplication a(argc, argv);
     gui_markdown w;
+
+    #ifdef Q_OS_LINUX
+        w.setWindowIcon(QIcon(":/icone/icon_linux.png"));
+    #endif
+
+    #ifdef Q_OS_WIN
+        w.setWindowIcon(QIcon(":/icone/icon_linux.png"));
+    #endif
+
     w.show();
     return a.exec();
 }
