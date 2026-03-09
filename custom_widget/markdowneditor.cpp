06f188e90d1607cac957cb468c7a1b37f3410b98
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2026-03-09 12:32:20 +0100

    Formatage de tout le markdown

--- custom_widget/markdowneditor.cpp
+++ custom_widget/markdowneditor.cpp
@@ -4,9 +4,4 @@ MarkdownEditor::MarkdownEditor(QWidget *parent)
     : QPlainTextEdit(parent)
 {
     highlighter = new MarkdownHighlighter(this->document());
-
-    // Optionnel : une police monospace pour l'aspect "code"
-    QFont font("Cascadia Code", 10);
-    font.setStyleHint(QFont::Monospace);
-    this->setFont(font);
 }
--- custom_widget/markdownhighlighter.cpp
+++ custom_widget/markdownhighlighter.cpp
@@ -9,28 +9,77 @@ MarkdownHighlighter::MarkdownHighlighter(QTextDocument *parent) :
 void MarkdownHighlighter::setupRules() {
     HighlightingRule rule;
 
-    // Titres (ex: # Titre)
+
     QTextCharFormat titleFormat;
     titleFormat.setFontWeight(QFont::Bold);
-    titleFormat.setForeground(Qt::blue);
-    rule.pattern = QRegularExpression("^#.*");
+
+    titleFormat.setFontPointSize(30);
+    rule.pattern = QRegularExpression("^#[^#].*");
     rule.format = titleFormat;
     rules.append(rule);
 
-    // Gras (ex: **gras**)
+
+    titleFormat.setFontPointSize(25);
+    rule.pattern = QRegularExpression("^##[^#].*");
+    rule.format = titleFormat;
+    rules.append(rule);
+
+    titleFormat.setFontPointSize(20);
+    rule.pattern = QRegularExpression("^###[^#].*");
+    rule.format = titleFormat;
+    rules.append(rule);
+
     QTextCharFormat boldFormat;
     boldFormat.setFontWeight(QFont::Bold);
     rule.pattern = QRegularExpression("\\*\\*.*\\*\\*");
     rule.format = boldFormat;
     rules.append(rule);
 
-    // Code (ex: `code`)
+    QTextCharFormat italicFormat;
+    italicFormat.setFontItalic(true);
+    rule.pattern = QRegularExpression("_[^_]+_");
+    rule.format = italicFormat;
+    rules.append(rule);
+
     QTextCharFormat codeFormat;
     codeFormat.setFontFixedPitch(true);
-    //codeFormat.setBackground(Qt::grey);
+    codeFormat.setBackground(QColor("#f0f0f0"));
     rule.pattern = QRegularExpression("`.*`");
     rule.format = codeFormat;
     rules.append(rule);
+
+    QTextCharFormat tableFormat;
+    tableFormat.setForeground(Qt::darkGray);
+    tableFormat.setFontWeight(QFont::Bold);
+    rule.pattern = QRegularExpression("\\|");
+    rule.format = tableFormat;
+    rules.append(rule);
+
+    QTextCharFormat headerFormat;
+    headerFormat.setForeground(Qt::darkMagenta);
+    headerFormat.setFontWeight(QFont::Bold);
+    rule.pattern = QRegularExpression("^\\s*\\|?[\\s\\-:]+\\|[\\s\\-:\\|]*$");
+    rule.format = tableFormat;
+    rules.append(rule);
+
+    rule.pattern = QRegularExpression("^\\|.*\\|$");
+    rule.format = headerFormat;
+    rules.append(rule);
+
+    QTextCharFormat lineFormat;
+    lineFormat.setForeground(Qt::gray);
+    lineFormat.setFontWeight(QFont::Bold);
+    rule.pattern = QRegularExpression("^(\\s*[-*_\\s]){3,}\\s*$");
+    rule.format = lineFormat;
+    rules.append(rule);
+
+    QTextCharFormat linkFormat;
+    linkFormat.setForeground(Qt::blue);
+    linkFormat.setFontUnderline(true);
+
+    QTextCharFormat imageFormat;
+    imageFormat.setForeground(QColor("#228B22"));
+    imageFormat.setFontWeight(QFont::Bold);
 }
 
 void MarkdownHighlighter::highlightBlock(const QString &text) {
